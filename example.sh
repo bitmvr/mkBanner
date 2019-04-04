6f6a9c40b53b7ef6b6a7da57a75a062bc0dbd16a
Jesse Riddle <jriddle@salesforce.com>
2019-04-04 11:16:49 -0400

    Updated

--- example.sh
+++ example.sh
@@ -2,6 +2,6 @@
 
 source "$(dirname $0)/make-banner.sh"
 
-mkBanner "=" "30" " THIS IS A TEST MESSAGE"
+mkBanner "-" "100" " THIS IS A TEST MESSAGE"
 
 mkBanner "=" "50" "THIS IS A FIRST LINE MESSAGE.\n THIS IS A SECOND LINE MESSAGE.\n THIS IS A THIRD LIND MESSAGE."
